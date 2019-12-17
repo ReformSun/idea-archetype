c93f00f90b258e93832e9ce56f3a77d6eda5bc70
ideaS <ideaS@sunjihuadeMacBook-Pro.local>
2019-12-17 12:57:26 +0800

    d

---
+++ src/main/java/com/sunny/SunnyMain.java
@@ -0,0 +1,7 @@
+package com.sunny;
+
+public class SunnyMain {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
---
+++ src/main/scala/com/sunny/ScalaSunnyMain.scala
@@ -0,0 +1,8 @@
+package com.sunny
+
+object ScalaSunnyMain {
+  def main(args: Array[String]): Unit = {
+    println("Hello world")
+  }
+
+}
